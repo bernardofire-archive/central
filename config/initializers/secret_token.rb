b8cd073af6575111ca172649a2693af61a93780f
Bernardo B. Marques <bernardo.fire@gmail.com>
2014-07-23 09:40:19 -0300

    open house

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Centralestudantil::Application.config.secret_key_base = '848cff0e3c4eb415fcfbccf8f8a0dc492a51faae10de5dfa4a2d29e70b58160bb96e1da3be5a50d8e753699331fe02ea6d5124fdca9a16676ce0a2085aeb0aea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Centralestudantil::Application.config.session_store :cookie_store, key: '_centralestudantil_session'
