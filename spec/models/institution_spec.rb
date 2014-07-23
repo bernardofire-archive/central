2fa68dcffc67c71d9aec9a5c9290d718b2b4ee8c
Bernardo B. Marques <bernardo.fire@gmail.com>
2014-07-23 17:08:28 -0300

    fix test

--- spec/models/institution_spec.rb
+++ spec/models/institution_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Institution, :type => :model do
   it 'should ' do
-    ins = Institution.new('foo')
+    ins = Institution.new(name: 'foo')
     expect(ins.name).to be_eql('foo')
   end
 end
