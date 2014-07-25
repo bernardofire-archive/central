76eff542bf63e58ca06d9c6f87d97bc058a7f34d
Bernardo B. Marques <bernardo.fire@gmail.com>
2014-07-25 08:13:48 -0300

    validate classroom name uniqueness

--- spec/models/classroom_spec.rb
+++ spec/models/classroom_spec.rb
@@ -6,6 +6,14 @@ RSpec.describe Classroom, :type => :model do
     expect(classroom.name).to be_eql('a')
   end
 
+  describe 'name' do
+    it 'validate uniqueness' do
+    Classroom.create(name: 'a')
+    classroom = Classroom.create(name: 'a')
+    expect(classroom.save).to be_eql(false)
+    end
+  end
+
   it 'test name length validation' do
   end
 end
