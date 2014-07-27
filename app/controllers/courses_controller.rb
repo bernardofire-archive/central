a4d8d9e6f06820e2be0bd930a45eb52f6f87b054
Bernardo B. Marques <bernardo.fire@gmail.com>
2014-07-26 21:22:42 -0300

    courses controller and routes

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,28 @@
 class CoursesController < ApplicationController
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(params[:course])
+    if @course.save
+      redirect_to @course
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  def edit
+    @course = Course.find(params[:id])
+  end
+
+  def update
+    @course = Course.find(params[:id])
+    @course.update!(params[:course])
+    redirect_to @course
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Centralestudantil::Application.routes.draw do
   devise_for :users
   resources :institutions
+  resources :courses
   root to: 'pages#home'
 end
