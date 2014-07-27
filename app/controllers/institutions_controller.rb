01e503f991225e5165dcf06077f7c728832a99ab
Bernardo B. Marques <bernardo.fire@gmail.com>
2014-07-26 21:16:58 -0300

    institution controller and views

--- app/controllers/institutions_controller.rb
+++ app/controllers/institutions_controller.rb
@@ -1,2 +1,28 @@
 class InstitutionsController < ApplicationController
+  def new
+    @institution = Institution.new
+  end
+
+  def create
+    @institution = Institution.new(params[:institution])
+    if @institution.save
+      redirect_to @institution
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @institution = Institution.find(params[:id])
+  end
+
+  def edit
+    @institution = Institution.find(params[:id])
+  end
+
+  def update
+    @institution = Institution.find(params[:id])
+    @institution.update!(params[:institution])
+    redirect_to @institution
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Centralestudantil::Application.routes.draw do
   devise_for :users
+  resources :institutions
   root to: 'pages#home'
 end
