2506d39991d8f762f83bdb17f678b0fa6424e470
Bernardo B. Marques <bernardo.fire@gmail.com>
2014-08-25 18:48:09 -0300

    add routes and controller for periods

--- app/controllers/periods_controller.rb
+++ app/controllers/periods_controller.rb
@@ -1,2 +1,28 @@
 class PeriodsController < ApplicationController
+  def new
+    @period = Period.new
+  end
+
+  def create
+    @period = Period.create(params[:id])
+  end
+
+  def show
+    @period = Period.find(params[:id])
+  end
+
+  def edit
+    @period = Period.find(params[:id])
+  end
+
+  def update
+    @period = Period.find(params[:id])
+    @period.update!(params[:period])
+    redirect_to @period
+  end
+
+  def destroy
+    Period.find(params[:id]).destroy!
+    redirect_to periods_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Centralestudantil::Application.routes.draw do
   devise_for :users
   resources :institutions
   resources :courses
+  resources :periods
   root to: 'pages#home'
 end
