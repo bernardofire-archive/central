55ba737bfa43be24f99866f456516f1178162169
Bernardo B. Marques <bernardo.fire@gmail.com>
2014-08-26 20:54:42 -0300

    course has many subject

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,7 @@
 class Course < ActiveRecord::Base
   attr_accessible :name, :institution
   has_many :periods
+  has_many :subjects
   belongs_to :institution
   validates :name, :institution, presence: true
 end
